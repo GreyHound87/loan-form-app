b6a55a6b8856b9e02606dfbe72f558b92a969d2d
Sergei Smirnov <sasmirnov87@gmail.com>
2024-08-07 20:54:29 +0300

    Basic application layout

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Loan Form App
 
 The project is built on Vite. (In my opinion, this is the best solution at the moment)
+
+During development, I used ready-made Ant Design components to speed up development.
+
+The maximum width of the layout is 1280px. At higher resolutions, dynamic indents appear to the left and right of the layout, the layout itself is always in the center.
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import { Layout } from 'antd';
 
-export function App() {
-    return <span>App</span>;
+const { Header, Footer, Content } = Layout;
+
+export function App(): JSX.Element {
+    return (
+        <Layout
+            style={{
+                maxWidth: '1280px',
+                margin: '0 auto',
+            }}
+        >
+            <Header>
+                <h1 style={{ color: 'rgba(255, 255, 255, 0.95)', margin: '0' }}>Loan Form App</h1>
+            </Header>
+            <Content>Content</Content>
+            <Footer>Footer</Footer>
+        </Layout>
+    );
 }
